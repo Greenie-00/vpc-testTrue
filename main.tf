fc23cec29daf82846c0fe25ba2d2d39b008fdb56
larry <tsporthd@gmail.com>
2024-09-05 18:41:48 -0400

    Modifications to support class

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_vpc" "llptest-aws-vpc" {
   cidr_block         = "10.0.0.0/16"
   enable_dns_support = "true" #gives you an internal domain name
   enable_dns_hostnames = "true" #gives you an internal host name
-  enable_classiclink = "false"
   instance_tenancy   = "default"
   tags = {
     Name = "llptest-aws-vpc"
