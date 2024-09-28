4a6e480edcd87675b53a2a5e3dda71b8d416ad85
larry <tsporthd@gmail.com>
2024-09-28 15:16:09 -0400

    refactor

--- main.tf
+++ main.tf
@@ -1,38 +1,10 @@
-resource "aws_vpc" "llptest-aws-vpc" {
-  cidr_block         = var.aws_vpc_cidr_block
-  enable_dns_support = "true" #gives you an internal domain name
-  enable_dns_hostnames = "true" #gives you an internal host name
-  instance_tenancy   = "default"
-  tags = {
-    Name = "llptest-aws-vpc"
-  }
+
+
+module "my-vpc"{
+  source = "./modules/vpc"
+  aws_vpc_cidr_block = var.aws_vpc_cidr_block
+  aws_public_subnet_cidr = var.aws_public_subnet_cidr
+  aws_private_subnet_cidr = var.aws_private_subnet_cidr
 }
 
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.llptest-aws-vpc.id
-  cidr_block = var.aws_public_subnet_cidr
-
-  tags = {
-    Name = "public_subnet"
-  }
-}
-
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.llptest-aws-vpc.id
-  cidr_block = var.aws_private_subnet_cidr
-
-  tags = {
-    Name = "private_subnet"
-  }
-}
-
-resource "aws_internet_gateway" "llp_test_igw" {
-  vpc_id = aws_vpc.llptest-aws-vpc.id
-
-  tags = {
-    Name = "internet gateway"
-  }
-}
 
--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
-provider "aws" {
-  region = var.aws_region
+provider aws  {
+  region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   allowed_account_ids = ["590184034896"]
 }
+
