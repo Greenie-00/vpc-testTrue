6631a4866fea7a929b451e8212e3c9aa63b846cb
tsporthd <tsporthd@gmail.com>
2025-04-25 18:22:47 -0400

    Reformat

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "llptest-aws-vpc" {
-  cidr_block         = var.aws_vpc_cidr_block
+  cidr_block       = var.aws_vpc_cidr_block
   enable_dns_support = "true" #gives you an internal domain name
   enable_dns_hostnames = "true" #gives you an internal host name
-  instance_tenancy   = "default"
+  instance_tenancy = "default"
   tags = {
     Name = "llptest-aws-vpc"
   }
