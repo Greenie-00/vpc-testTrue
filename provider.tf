b40f224c826c7ab85e11388a877850af14c52c56
Greenie-00 <grant_rollins06@protonmail.com>
2025-10-07 16:57:40 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region              = var.aws_region
   access_key          = var.aws_access_key
   secret_key          = var.aws_secret_key
-  allowed_account_ids = ["590184034896"]
+  allowed_account_ids = ["053155682228"]
 }
